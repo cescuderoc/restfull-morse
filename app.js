7ef48488c52073e76bc3b5c218df91e6fb07d671
rhuenchual <roger.huenchual@gmail.com>
2023-04-28 12:33:26 -0400

    Cambios para estabilizar version

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const usuariosRoute = require('./routes/usuariosRoute');
-const { sqliteMiddleware } = require('./middlewares/sqliteMiddleware');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+const port = process.env.PORT || 3000;
+const router = express.Router();
 
-app.use(express.json());
-app.use(sqliteMiddleware);
-
-app.use('/usuarios', usuariosRoute);
-
-app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+router.get('/', (req, res) => {
+  res.json({ message: 'API RESTful funcionando correctamente' });
 });
+
+router.route('/usuarios')
+  .get((req, res) => {
+    // Aquí se obtendrían los usuarios desde una base de datos o un archivo, por ejemplo
+    const usuarios = [{ id: 1, nombre: 'Juan' }, { id: 2, nombre: 'María' }];
+    res.json(usuarios);
+  })
+  .post((req, res) => {
+    // Aquí se crearía un nuevo usuario con los datos que llegan en el cuerpo de la petición
+    const nuevoUsuario = { id: 3, nombre: req.body.nombre };
+    res.status(201).json(nuevoUsuario);
+  });
+
+router.route('/usuarios/:id')
+  .get((req, res) => {
+    // Aquí se buscaría y devolvería el usuario con el ID especificado en la URL
+    const usuario = { id: req.params.id, nombre: 'Juan' };
+    res.json(usuario);
+  })
+  .put((req, res) => {
+    // Aquí se actualizaría el usuario con el ID especificado en la URL con los datos que llegan en el cuerpo de la petición
+    const usuarioActualizado = { id: req.params.id, nombre: req.body.nombre };
+    res.json(usuarioActualizado);
+  })
+  .delete((req, res) => {
+    // Aquí se eliminaría el usuario con el ID especificado en la URL
+    res.status(204).send();
+  });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log(`API RESTful funcionando en http://localhost:${port}/api`);
